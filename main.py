8803c425c4236b4263afaee3efd9fbc681fe2892
kostya shilkrot <kostya.shilkrot@gmail.com>
2025-08-21 20:00:49 +0300

    added new files

--- main.py
+++ main.py
@@ -12,7 +12,18 @@ def fetch_hh_vacancies(url: str, page: int = 0):
     }
 
     resp = requests.get(url=url, params=query_param)
+    if resp.status_code != 200:
+        print(f"Error reply { resp.status_code= }")
     json_data = resp.json()
+    return json_data
+
+def main():
+    res = fetch_hh_vacancies(url=url)
+    print(res)
+
+if __name__ == "__main__":
+      main()
+
 
 
 
